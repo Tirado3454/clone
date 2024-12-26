7390abec90dd8760b9765341eb77f97ad0b99905
Tirado3454 <augustotirado@gmail.com>
2024-12-26 11:39:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,37 +9,47 @@ from utils.contextualizacao import contextualizacao_page_function
 # Configuração inicial
 st.set_page_config(page_title="MHD no Xadrez", layout="wide")
 
+# Inicializar estado de navegação
+if "active_tab" not in st.session_state:
+    st.session_state["active_tab"] = "Apresentação"
+
+# Função para alterar o estado ativo
+def set_active_tab(tab_name):
+    st.session_state["active_tab"] = tab_name
+
 # Dividindo o layout em duas colunas
 col1, col2 = st.columns([1, 3])
 
 # Coluna 1 - Navegação
 with col1:
     st.markdown("### Textos Explicativos")
-    texto_opcoes = ["Apresentação", "Contextualização"]
-    texto_selecao = st.radio("Selecione:", texto_opcoes, key="text_navigation")
+    if st.button("Apresentação", key="btn_apresentacao"):
+        set_active_tab("Apresentação")
+    if st.button("Contextualização", key="btn_contextualizacao"):
+        set_active_tab("Contextualização")
 
     st.markdown("### Funcionalidades")
-    func_opcoes = [
-        "Modelo Hipotético-Dedutivo",
-        "Editor de Tabuleiro",
-        "Banco de Frases",
-        "Exportar Dados"
-    ]
-    func_selecao = st.radio("Escolha uma funcionalidade:", func_opcoes, key="func_navigation")
+    if st.button("Modelo Hipotético-Dedutivo", key="btn_mhd"):
+        set_active_tab("Modelo Hipotético-Dedutivo")
+    if st.button("Editor de Tabuleiro", key="btn_tabuleiro"):
+        set_active_tab("Editor de Tabuleiro")
+    if st.button("Banco de Frases", key="btn_frases"):
+        set_active_tab("Banco de Frases")
+    if st.button("Exportar Dados", key="btn_exportar"):
+        set_active_tab("Exportar Dados")
 
 # Coluna 2 - Conteúdo
 with col2:
-    # Verificar se um texto ou uma funcionalidade foi selecionado
-    if texto_selecao == "Apresentação":
+    if st.session_state["active_tab"] == "Apresentação":
         apresentacao_function()
-    elif texto_selecao == "Contextualização":
+    elif st.session_state["active_tab"] == "Contextualização":
         contextualizacao_page_function()
-    elif func_selecao == "Modelo Hipotético-Dedutivo":
+    elif st.session_state["active_tab"] == "Modelo Hipotético-Dedutivo":
         mhd_function()
-    elif func_selecao == "Editor de Tabuleiro":
+    elif st.session_state["active_tab"] == "Editor de Tabuleiro":
         board_editor_function()
-    elif func_selecao == "Banco de Frases":
+    elif st.session_state["active_tab"] == "Banco de Frases":
         phrase_bank_function()
-    elif func_selecao == "Exportar Dados":
+    elif st.session_state["active_tab"] == "Exportar Dados":
         generate_pdf()
         generate_csv()
