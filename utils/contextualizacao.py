ca4996a835e7f1b82a83834a08fdd39fcfa20eef
Tirado3454 <augustotirado@gmail.com>
2024-12-26 11:00:35 -0300

    Add files via upload

--- utils/contextualizacao.py
+++ utils/contextualizacao.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-def contextualizacao_page_function():
+def contextualizacao_function():
     st.title("Contextualização")
-    
+
     st.markdown("""
     ## A Importância do Método Hipotético-Dedutivo (MHD) no Contexto Atual
     
