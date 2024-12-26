984d1cb5fd12fa8e0076b74763fe517f4e26baa0
Tirado3454 <augustotirado@gmail.com>
2024-12-26 12:08:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,48 +3,50 @@ from utils.maintexto import mhd_function
 from utils.tabuleiro import board_editor_function
 from utils.frases import phrase_bank_function
 from utils.export import generate_pdf, generate_csv
-from utils.apresentacao import apresentacao_function  # Página de Apresentação
-from utils.contextualizacao import contextualizacao_page_function  # Página de Contextualização
+from utils.apresentacao import apresentacao_function
+from utils.contextualizacao import contextualizacao_page_function
 
 # Configuração inicial
-st.set_page_config(page_title="MHD no Xadrez", layout="centered")
+st.set_page_config(page_title="MHD no Xadrez", layout="wide")
 
-# Título Principal
-st.title("MENU PRINCIPAL")
+# Dividindo o layout em duas colunas
+col1, col2 = st.columns([1, 3])
 
-# Seção de Textos Estáticos
-st.header("Informações e Introdução")
-with st.container():
-    st.subheader("Escolha um Tópico:")
-    opcoes_textos = ["Apresentação", "Contextualização"]
-    texto_escolhido = st.radio("", opcoes_textos, key="text_section")
+# Coluna 1 - Navegação
+with col1:
+    st.markdown("### Menu de Navegação")
+    st.markdown("#### Textos")
+    texto_selecionado = st.radio(
+        "Escolha uma página:",
+        ["Apresentação", "Contextualização"],
+        key="text_navigation"
+    )
 
-if texto_escolhido == "Apresentação":
-    apresentacao_function()
-elif texto_escolhido == "Contextualização":
-    contextualizacao_page_function()
+    st.markdown("#### Funcionalidades")
+    funcionalidade_selecionada = st.radio(
+        "Escolha uma funcionalidade:",
+        [
+            "Modelo Hipotético-Dedutivo",
+            "Editor de Tabuleiro",
+            "Banco de Frases",
+            "Exportar Dados"
+        ],
+        key="function_navigation"
+    )
 
-# Separação visual com linha horizontal
-st.markdown("---")
+# Coluna 2 - Conteúdo
+with col2:
+    if texto_selecionado == "Apresentação":
+        apresentacao_function()
+    elif texto_selecionado == "Contextualização":
+        contextualizacao_page_function()
 
-# Seção de Funcionalidades Interativas
-st.header("Funcionalidades Interativas")
-with st.container():
-    st.subheader("Selecione uma Ferramenta:")
-    opcoes_funcionalidades = [
-        "Modelo Hipotético-Dedutivo",
-        "Editor de Tabuleiro",
-        "Banco de Frases",
-        "Exportar Dados",
-    ]
-    funcionalidade_escolhida = st.radio("", opcoes_funcionalidades, key="interactive_section")
-
-if funcionalidade_escolhida == "Modelo Hipotético-Dedutivo":
-    mhd_function()
-elif funcionalidade_escolhida == "Editor de Tabuleiro":
-    board_editor_function()
-elif funcionalidade_escolhida == "Banco de Frases":
-    phrase_bank_function()
-elif funcionalidade_escolhida == "Exportar Dados":
-    generate_pdf()
-    generate_csv()
+    if funcionalidade_selecionada == "Modelo Hipotético-Dedutivo":
+        mhd_function()
+    elif funcionalidade_selecionada == "Editor de Tabuleiro":
+        board_editor_function()
+    elif funcionalidade_selecionada == "Banco de Frases":
+        phrase_bank_function()
+    elif funcionalidade_selecionada == "Exportar Dados":
+        generate_pdf()
+        generate_csv()
